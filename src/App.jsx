21ba0ac55088b5aa4ff18817ce8e11e95eee0795
emmanuel sam <fndm4@gmail.com>
2025-09-02 13:15:57 +0100

    set the footer and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import About from "./pages/about/About";
 import Services from "./pages/services/Services";
 import Project from "./pages/project/Project";
 import Contact from "./pages/contact/Contact";
-import Footer from './components/footer/Footer'
+import Footer from "./components/footer/Footer";
+import ScrollToTop from "./components/top/ScrollToTop";
+import Scroll from "./components/top/Scroll";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
+      <Scroll />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
-          <Route path="/our-services" element={<Services />} />
-          <Route path="/our-projects" element={<Project />} />
-          <Route path="/our-projects" element={<Project />} />
-          <Route path="/contact-us" element={<Contact />} />
+        <Route path="/our-services" element={<Services />} />
+        <Route path="/our-projects" element={<Project />} />
+        <Route path="/our-projects" element={<Project />} />
+        <Route path="/contact-us" element={<Contact />} />
       </Routes>
       <Footer />
     </>
