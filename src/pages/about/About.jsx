4cded69ad173c5047e1e4b6ced5e06e4a51dfc77
oyis12 <oyivosam12@gmail.com>
2025-09-03 15:40:45 +0100

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="w-full h-[400px] bg-[url('assets/about2.jpg')] bg-cover  bg-bottom relative lg:-top-[5.2rem] sm:top-[20px] flex flex-col justify-center items-center text-center px-4 bg-no-repeat sm:bg-scroll lg:bg-fixed">
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black/50"></div>
+
+        {/* Content */}
+        <div className="relative z-10 text-white space-y-4">
+          <h2 className="text-5xl font-bold">About Us</h2>
+          <h3 className="text-4xl font-medium">Shaping the Future with Expertise and Innovation</h3>
+          <p className="max-w-2xl mx-auto text-2xl lg:text-base leading-relaxed font-semibold">
+            Delivering excellence across civil engineering, oil and gas, and global project management. 
+            Our expertise drives innovation, fuels progress, and builds a sustainable future.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
