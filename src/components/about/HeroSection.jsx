e54154eb5d2ad04a85e73c8feed393f129a28dde
oyis12 <oyivosam12@gmail.com>
2025-09-05 11:27:00 +0100

    updated

--- src/components/about/HeroSection.jsx
+++ src/components/about/HeroSection.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
     <div className="h-[400px] bg-[url('assets/about2.jpg')] bg-no-repeat bg-cover bg-center relative">
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/50 h-[400px] flex justify-center items-center flex-col">
-        <div className="relative z-10 text-white space-y-4 px-4 sm:px-6 md:px-8 text-center">
+        <div className="relative z-10 text-white space-y-4 px-4 sm:px-6 md:px-8 lg:text-center">
           <h2 className="text-4xl sm:text-5xl font-bold">About Us</h2>
           <h3 className="text-2xl sm:text-3xl lg:text-4xl font-medium">
             Shaping the Future with Expertise and Innovation
