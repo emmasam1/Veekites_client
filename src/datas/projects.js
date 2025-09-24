f1ec7eebe9db5aafbb7ae810165c07360e4e0f53
oyis12 <oyivosam12@gmail.com>
2025-09-24 10:10:10 +0100

    update

--- src/datas/projects.js
+++ src/datas/projects.js
@@ -1,13 +1,4 @@
-import prj from "../assets/prj.jpeg";
-import prj1 from "../assets/prj1.jpeg";
 import project1 from "../assets/project-11.jpg";
-import project2 from "../assets/project-10.jpg";
-import project3 from "../assets/project-9.jpg";
-
-import gallery1 from "../assets/project-11.jpg";
-import gallery2 from "../assets/project-10.jpg";
-import gallery3 from "../assets/project-9.jpg";
-import gallery4 from "../assets/project-11.jpg";
 import gallery5 from "../assets/project-9.jpg";
 import p1 from "../assets/p1.jpeg";
 import p2 from "../assets/p2.jpeg";
@@ -15,7 +6,6 @@ import p3 from "../assets/p3.jpeg";
 import p4 from "../assets/p4.jpeg";
 import p5 from "../assets/p5.jpeg";
 import p6 from "../assets/p6.jpeg";
-import p7 from "../assets/p7.jpeg";
 import p8 from "../assets/p8.jpeg";
 import p9 from "../assets/p9.jpeg";
 import p10 from "../assets/p10.jpeg";
@@ -81,149 +71,6 @@ export const projects = [
     gallery: [p20, p21, p22],
     isLatest: false,
   },
-  // {
-  //   id: 2,
-  //   title: "Work Management",
-  //   client: "Delta Group",
-  //   location: "Lagos, Nigeria",
-  //   area: "320,000 sf",
-  //   year: 2021,
-  //   budget: "$180,000,000",
-  //   architect: "Miller & James",
-  //   sector: "Corporate, ICT",
-  //   description:
-  //     "Sustainable workspaces designed to improve collaboration and efficiency across departments, featuring modern infrastructure and flexible layouts for future expansions.",
-  //   mainImage: project2,
-  //   gallery: [gallery2, gallery3, gallery4, gallery5, gallery1],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 3,
-  //   title: "Creation of a mini stadium for the Torogbene community",
-  //   client: "Zenith Materials Ltd",
-  //   location: "Delta State",
-  //   area: "Burutu Local Government",
-  //   year: 2018,
-  //   budget: "$220,000,000",
-  //   architect: "Brown & Partners",
-  //   sector: "Industrial, R&D",
-  //   description:
-  //     "This stadium was built for the Torogbene community in Burutu Local Government, Delta State. The mini stadium serves as a multi-use facility for local sports, community events, and youth activities, promoting physical fitness and community engagement.",
-  //   sub_dec: [
-  //     {
-  //       id: 1,
-  //       text: "Facilities Included:The stadium features a playing field, seating for spectators, changing rooms, and rest areas to support various sporting events.",
-  //     },
-  //     {
-  //       id: 2,
-  //       text: "Community Impact: This project aims to enhance the quality of life for residents by providing a safe and accessible venue for recreation and social gatherings.",
-  //     },
-  //     {
-  //       id: 3,
-  //       text: "Future Plans: There are intentions to organize regular sports tournaments and community activities to foster teamwork and unity within the Torogbene community.",
-  //     },
-  //   ],
-  //   mainImage: project3,
-  //   gallery: [gallery3, gallery1, gallery2, gallery5, gallery4],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 4,
-  //   title: "Power and Energy",
-  //   client: "EcoEnergy Corp",
-  //   location: "Cape Town, South Africa",
-  //   area: "600,000 sf",
-  //   year: 2020,
-  //   budget: "$300,000,000",
-  //   architect: "Green Solutions",
-  //   sector: "Energy, Utilities",
-  //   description:
-  //     "Development of renewable energy infrastructure with a focus on wind and solar integration, designed to reduce carbon emissions and power thousands of homes sustainably.",
-  //   mainImage: project1,
-  //   gallery: [gallery1, gallery4, gallery2, gallery3, gallery5],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 5,
-  //   title: "Land Minning",
-  //   client: "Pacific Minerals",
-  //   location: "Perth, Australia",
-  //   area: "700,000 sf",
-  //   year: 2017,
-  //   budget: "$280,000,000",
-  //   architect: "Stone & Build",
-  //   sector: "Mining, Industrial",
-  //   description:
-  //     "Large-scale mining project focused on resource extraction with sustainable practices, balancing industrial growth while maintaining environmental responsibility.",
-  //   mainImage: project3,
-  //   gallery: [gallery5, gallery3, gallery1, gallery4, gallery2],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 6,
-  //   title: "Work Management",
-  //   client: "Skyline Builders",
-  //   location: "Dubai, UAE",
-  //   area: "450,000 sf",
-  //   year: 2022,
-  //   budget: "$260,000,000",
-  //   architect: "Modern Edge",
-  //   sector: "Corporate, High-rise",
-  //   description:
-  //     "Innovative office tower designed with smart building technology and green certification, creating a modern landmark for corporate headquarters.",
-  //   mainImage: project2,
-  //   gallery: [gallery2, gallery1, gallery3, gallery5, gallery4],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 7,
-  //   title: "Material Engineering",
-  //   client: "CoreTech Industries",
-  //   location: "Berlin, Germany",
-  //   area: "380,000 sf",
-  //   year: 2021,
-  //   budget: "$210,000,000",
-  //   architect: "FutureBuild GmbH",
-  //   sector: "Industrial, R&D",
-  //   description:
-  //     "Cutting-edge research facility specializing in lightweight composites and sustainable materials, supporting Europe’s growing manufacturing sector.",
-  //   mainImage: project3,
-  //   gallery: [gallery3, gallery5, gallery1, gallery4, gallery2],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 8,
-  //   title: "Power and Energy",
-  //   client: "SolarNext",
-  //   location: "Madrid, Spain",
-  //   area: "500,000 sf",
-  //   year: 2019,
-  //   budget: "$275,000,000",
-  //   architect: "SunTech Architects",
-  //   sector: "Energy, Renewables",
-  //   description:
-  //     "A solar farm development project delivering sustainable electricity to urban and rural areas, pushing forward Spain’s renewable energy agenda.",
-  //   mainImage: project1,
-  //   gallery: [gallery4, gallery1, gallery3, gallery5, gallery2],
-  //   isLatest: false,
-  // },
-  // {
-  //   id: 9,
-  //   title: "Work Management",
-  //   client: "Prime Developments",
-  //   location: "Toronto, Canada",
-  //   area: "360,000 sf",
-  //   year: 2023,
-  //   budget: "$190,000,000",
-  //   architect: "UrbanSpace Design",
-  //   sector: "Corporate, Technology",
-  //   description:
-  //     "Modern corporate campus integrating open office spaces, digital infrastructure, and eco-friendly construction standards to promote future-ready enterprises.",
-  //   mainImage: project2,
-  //   gallery: [gallery2, gallery3, gallery5, gallery1, gallery4],
-  //   isLatest: false,
-  // },
-  // From Latest Projects
   {
     id: 10,
     title:
@@ -244,15 +91,15 @@ export const projects = [
   {
     id: 11,
     title: "Mini stadium",
-    client: "EcoFuture Energy",
-    location: "Nairobi, Kenya",
+    client: "Torogbene community",
+    location: "Torogbene community, Burutu Local Government, Delta State.",
     area: "620,000 sf",
     year: 2023,
     budget: "$270,000,000",
-    architect: "Sustain Design Group",
-    sector: "Energy, Renewables",
+    architect: "Veekites Global Services",
+    sector: "Construction",
     description:
-      "Development of a mini stadium for community sports and events, promoting local engagement and physical activity with modern amenities and sustainable design.",
+      "This stadium was built for the Torogbene community in Burutu Local Government, Delta State. The mini stadium serves as a multi-use facility for local sports, community events, and youth activities, promoting physical fitness and community engagement./n/ Facilities Included:The stadium features a playing field, seating for spectators, changing rooms, and rest areas to support various sporting events./n/ Community Impact: This project aims to enhance the quality of life for residents by providing a safe and accessible venue for recreation and social gatherings./n/ Future Plans: There are intentions to organize regular sports tournaments and community activities to foster teamwork and unity within the Torogbene community.",
     mainImage: p9,
     gallery: [p8, p10, gallery5, p11, p12],
     isLatest: true,
